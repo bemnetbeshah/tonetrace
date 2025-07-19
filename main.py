993c4526bbdd457f77bd05ee95fb3fc9d6475864
Bemnet Beshah <bemnetbeshah@gmail.com>
2025-07-19 16:54:39 -0400

    a working POST /analyze route

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message" : "You are now using tonetrace API"}
+
+class textInput(BaseModel):
+    text: str
+
+@app.post("/analyze")
+def analyze_text(input_data: textInput):
+    return {"message": "Text received", "input_length": len(input_data.text)}
 
